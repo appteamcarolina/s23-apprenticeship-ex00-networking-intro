15012bdf2b5cc823bf1977c72c6f4e3c185f07e4
Sam Shi <samrshi@outlook.com>
2023-01-24 22:02:07 -0500

    Initial Commit

---
+++ ex00-simplenetworking/ex00_simplenetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ex00_simplenetworkingApp.swift
+//  ex00-simplenetworking
+//
+//  Created by Samuel Shi on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct ex00_simplenetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
